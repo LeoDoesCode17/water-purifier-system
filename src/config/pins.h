f21c8a1a316bd050ad3dc79b5c36a8fe21cb29a2
leonardo <leocode1079@gmail.com>
2026-04-03 23:42:28 +0800

    fix: fixing typo in ECHO pin 1

--- src/config/pins.h
+++ src/config/pins.h
@@ -8,7 +8,7 @@ namespace pins {
     constexpr int PUMP_PIN = 6;
     constexpr int GEAR_MOTOR_PIN = 7;
     constexpr int TRIG_1_PIN = 10;
-    constexpr int ECHI_1_PIN = 11;
+    constexpr int ECHO_1_PIN = 11;
     constexpr int TRIG_2_PIN = 12;
     constexpr int ECHO_2_PIN = 13;
 }
