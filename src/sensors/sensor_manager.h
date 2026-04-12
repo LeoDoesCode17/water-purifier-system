cd790f5069499201970ad029d9a59442cebbf13d
leonardo <leocode1079@gmail.com>
2026-04-12 15:28:06 +0800

    feat: define header file for sensor manager

---
+++ src/sensors/sensor_manager.h
@@ -0,0 +1,8 @@
+#ifndef SENSORS_SENSOR_MANAGER_H
+#define SENSORS_SENSOR_MANAGER_H
+namespace sensor_manager {
+    void initialize_all();
+    float get_distance_ultrasonic_1();
+    float get_distance_ultrasonic_2();
+}
+#endif
