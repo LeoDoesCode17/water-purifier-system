50f04b76f069a037775f5fe4237f9c26a8881666
leonardo <leocode1079@gmail.com>
2026-04-12 17:31:28 +0800

    feat: get distance ultrasonic 1 and 2 and print in serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "config/pins.h"
+#include "config/constants.h"
+#include "sensors/sensor_manager.h"
+#include "actuators/actuator_manager.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(constant::BAUD_RATE);
+  sensor_manager::initialize_all();
+  actuator_manager::initialize();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float distance_1 = sensor_manager::get_distance_ultrasonic_1();
+  float distance_2 = sensor_manager::get_distance_ultrasonic_2();
+  Serial.print("Distance sensor 1: ");
+  Serial.println(distance_1);
+  Serial.print("Distance sensor 2: ");
+  Serial.println(distance_2);
+  delay(100);
 }
