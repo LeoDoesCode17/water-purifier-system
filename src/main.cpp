8af4956ec2f47ab2fd56fc6b8fe4e245c9ea6590
leonardo <leocode1079@gmail.com>
2026-04-13 16:02:39 +0800

    feat: read and print temperature and ultrasonic data

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,20 @@ void setup() {
 }
 
 void loop() {
+  sensor_manager::update_all();
   float distance_1 = sensor_manager::get_distance_ultrasonic_1();
   float distance_2 = sensor_manager::get_distance_ultrasonic_2();
+  float temperature_1 = sensor_manager::get_temperature_c_1();
+  float temperature_2 = sensor_manager::get_temperature_c_2();
+  Serial.println("============================");
   Serial.print("Distance sensor 1: ");
   Serial.println(distance_1);
   Serial.print("Distance sensor 2: ");
   Serial.println(distance_2);
-  delay(100);
+  Serial.print("Temperature sensor 1: ");
+  Serial.println(temperature_1);
+  Serial.print("Temperature sensor 2: ");
+  Serial.println(temperature_2);
+  Serial.println("============================");
+  delay(200);
 }
