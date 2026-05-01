5847b010e283bfe8f186bf5475fda170f3a3514b
leonardo <leocode1079@gmail.com>
2026-05-01 21:36:10 +0800

    feat; create a pusblish function for any payload

--- src/networks/mqtt.cpp
+++ src/networks/mqtt.cpp
@@ -1,7 +1,6 @@
 #include "networks/mqtt.h"
 #include "config/secrets.h"
 #include <PubSubClient.h>
-#include <ArduinoJson.h>  
 #include <WiFi.h>
 #include <Arduino.h>
 
@@ -28,6 +27,7 @@ namespace mqtt {
       mqttClient.setServer(secret::MQTT_BROKER, secret::MQTT_PORT);
       mqttClient.setCallback(callback);
     }
+
     bool connect() {
         Serial.print("[MQTT] Connecting to broker: ");
         Serial.println(secret::MQTT_BROKER);
@@ -56,31 +56,18 @@ namespace mqtt {
 
         return connected;
     }
+
     void loop() {
         mqttClient.loop();
     }
+
     bool is_connected() {
         return mqttClient.connected();
     }
-    bool publish(WaterQuality water_quality) {
-        StaticJsonDocument<256> doc;
-        doc["raw_tds"] = water_quality.ppm;
-        doc["raw_turbidity"] = water_quality.ntu;
-        doc["raw_temperature"] = water_quality.temperature;
-        doc["raw_ph"] = water_quality.ph;
-        doc["raw_water_level"] = water_quality.level;
 
-        char buffer[256];
-        size_t len = serializeJson(doc, buffer);
-
-        bool ok = mqttClient.publish(secret::MQTT_PUB_TOPIC, (uint8_t*)buffer, len);
-        if (ok) {
-            Serial.print("[MQTT] Published: ");
-            Serial.println(buffer);
-        } else {
-            Serial.println("[MQTT] Publish failed");
-        }
-        return ok;
+    bool publish(const char *topic, const uint8_t *payload, unsigned int plength) {
+        return mqttClient.publish(topic, payload, plength);
     }
+
 
 }
--- src/networks/mqtt.h
+++ src/networks/mqtt.h
@@ -1,11 +1,10 @@
 #ifndef NETWORKS_MQTT_H
 #define NETWORKS_MQTT_H
-#include "config/types.h"
 namespace mqtt {
     void initialize();
     bool connect();
     void loop();
     bool is_connected();
-    bool publish(WaterQuality water_quality);
+    bool publish(const char *topic, const uint8_t *payload, unsigned int plength);
 }
 #endif
