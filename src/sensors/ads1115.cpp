7560405832348b6dd2a717ea7012142378e23e8c
leonardo <leocode1079@gmail.com>
2026-05-13 13:25:36 +0800

    feat: modify counting voltage from ads raw

--- src/sensors/ads1115.cpp
+++ src/sensors/ads1115.cpp
@@ -3,9 +3,33 @@
 #include <Adafruit_ADS1X15.h>
 #include "config/pins.h"
 #include "config/constants.h"
+#include <Arduino.h>
 
 static Adafruit_ADS1115 ads_1; 
 static Adafruit_ADS1115 ads_2; 
+
+// utils function
+static float read_voltage_1(uint8_t channel, int samples = 20) {
+    long sum = 0;
+    for (size_t i = 0; i < samples; i++)
+    {
+        sum += ads_1.readADC_SingleEnded(channel);
+        delay(5);
+    }
+    float avg_raw = (float) sum / samples;
+    return avg_raw * constant::ADS_BIT_MV / 1000.0f;  
+} 
+
+static float read_voltage_2(uint8_t channel, int samples = 20) {
+    long sum = 0;
+    for (size_t i = 0; i < samples; i++)
+    {
+        sum += ads_2.readADC_SingleEnded(channel);
+        delay(5);
+    }
+    float avg_raw = (float) sum / samples;
+    return avg_raw * constant::ADS_BIT_MV / 1000.0f;  
+} 
 
 namespace ads1115 {
     bool initialize() {
@@ -36,14 +60,16 @@ namespace ads1115 {
     float get_voltage_value_1(int index) {
         // Validation to prevent out-of-bounds index
         if (index < 0 || index > 3) return 0.0f;
-        int16_t raw = ads_1.readADC_SingleEnded(index);
-        return ads_1.computeVolts(raw);
+        // int16_t raw = ads_1.readADC_SingleEnded(index);
+        // return ads_1.computeVolts(raw);
+        return read_voltage_1(index);
     }
 
     float get_voltage_value_2(int index) {
         if (index < 0 || index > 3) return 0.0f;
-        int16_t raw = ads_2.readADC_SingleEnded(index);
-        return ads_2.computeVolts(raw);
+        // int16_t raw = ads_2.readADC_SingleEnded(index);
+        // return ads_2.computeVolts(raw);
+        return read_voltage_1(index);
     }
     
     // You can also add a helper for raw values if needed
