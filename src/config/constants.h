69bfca8cdb6bde4b75defaa3d2435995c9808de0
leonardo <leocode1079@gmail.com>
2026-06-04 07:30:57 +0800

    feat: add constants

--- src/config/constants.h
+++ src/config/constants.h
@@ -1,15 +1,19 @@
 #ifndef CONFIG_CONSTANTS_H
 #define CONFIG_CONSTANTS_H
-#include <stdint.h> 
-namespace constant {
+#include <stdint.h>
+namespace constant
+{
     constexpr long BAUD_RATE = 115200;
     constexpr uint8_t ADS1115_ADDRESS_1 = 0x48;
     constexpr uint8_t ADS1115_ADDRESS_2 = 0x49;
-    constexpr uint8_t CH_TDS =2;
+    constexpr uint8_t CH_TDS = 2;
     constexpr uint8_t CH_TURBIDITY = 0;
     constexpr uint8_t CH_PH = 1;
     constexpr float PH_NEUTRAL_VOLTAGE = 2.5f;
     constexpr float PH_SLOPE = 0.1776f;
-    constexpr float ADS_BIT_MV = 0.125f; 
+    constexpr float ADS_BIT_MV = 0.125f;
+    constexpr float MAX_MIXING_TANK_LEVEL = 32.5;
+    constexpr unsigned long SENSOR_SAMPLE_COUNT = 200;
+    constexpr unsigned long MQTT_RECONNECT_INTERVAL = 5000;
 }
 #endif
