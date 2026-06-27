b110de1bba108e66278dde050976e5776a350cab
Leo <leonardonifinluri@gmail.com>
2026-06-27 23:03:28 +0800

    feat: add delay to simulate prediction process

--- src/ai/ai_manager.cpp
+++ src/ai/ai_manager.cpp
@@ -1,5 +1,5 @@
 #include "ai/ai_manager.h"
-
+#include <Arduino.h>
 static float moringa_dose_gram = 0.0;
 static float mixing_time_seconds = 0.0;
 namespace ai_manager
@@ -10,6 +10,7 @@ namespace ai_manager
     void predict(const WaterQuality &wq)
     {
         // TODO: implement the prediciton using WQ by modifying the value of moringa_dose_gram and mixing_time_seconds
+        delay(2000);
     }
     float get_moringa_dose_gram()
     {
