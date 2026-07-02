26c740923859fab645e29d0b8975c2704753f0ac
Leo <leonardonifinluri@gmail.com>
2026-07-02 09:16:58 +0800

    feat: remove namespace pins for non-display pin configuration and wrap using platform IO build flag

--- src/config/pins.h
+++ src/config/pins.h
@@ -1,16 +1,53 @@
-#ifndef CONFIG_PINS_H
-#define CONFIG_PINS_H
-namespace pins {
-    constexpr int SDA_PIN = 8;
-    constexpr int SCL_PIN = 9;
-    constexpr int SERVO_VALVE_PIN = 4;
-    constexpr int PUMP_1_PIN = 5;
-    constexpr int PUMP_2_PIN = 6;
-    constexpr int GEAR_MOTOR_PIN = 7;
-    constexpr int TRIG_1_PIN = 10;
-    constexpr int ECHO_1_PIN = 11;
-    constexpr int TRIG_2_PIN = 12;
-    constexpr int ECHO_2_PIN = 13;
-    constexpr int DQ_PIN = 14;
-}
+#pragma once
+
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+
+// TFT
+#define TFT_MISO 38
+#define TFT_MOSI 18
+#define TFT_SCLK 21
+#define TFT_CS 15
+#define TFT_DC 17
+#define TFT_RST 16
+#define TFT_BL 46
+
+// Sensors / actuators
+#define SDA_PIN 8
+#define SCL_PIN 9
+#define SERVO_VALVE_PIN 4
+#define PUMP_1_PIN 5
+#define PUMP_2_PIN 6
+#define GEAR_MOTOR_PIN 7
+#define TRIG_1_PIN 10
+#define ECHO_1_PIN 11
+#define TRIG_2_PIN 12
+#define ECHO_2_PIN 13
+#define DQ_PIN 14
+
+#elif defined(CONFIG_IDF_TARGET_ESP32)
+
+// TFT
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
+#define TFT_BL -1
+
+// Sensors / actuators
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define SERVO_VALVE_PIN 16
+#define PUMP_1_PIN 27
+#define PUMP_2_PIN 14
+#define GEAR_MOTOR_PIN 13
+#define TRIG_1_PIN 25
+#define ECHO_1_PIN 26
+#define TRIG_2_PIN 32
+#define ECHO_2_PIN 33
+#define DQ_PIN 17
+
+#else
+#error "Unsupported board target"
 #endif
