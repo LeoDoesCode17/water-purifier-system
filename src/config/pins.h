5eca2ccdd25e5931c2f65fb2ca0b74475298501a
Leonardo <leocode1079@gmail.com>
2026-07-18 16:39:01 +0800

    feat: move tft pin definition from pins.h to platform.ini

--- src/config/pins.h
+++ src/config/pins.h
@@ -1,45 +1,24 @@
 #pragma once
+// ESP32S#
+// // Sensors / actuators
+// #define SDA_PIN 8
+// #define SCL_PIN 9
+// #define SERVO_VALVE_PIN 4
+// #define PUMP_1_PIN 5
+// #define PUMP_2_PIN 6
+// #define GEAR_MOTOR_PIN 7
+// #define TRIG_1_PIN 10
+// #define ECHO_1_PIN 11
+// #define TRIG_2_PIN 12
+// #define ECHO_2_PIN 13
+// #define DQ_PIN 14
 
-#if defined(BOARD_ESP32S3_DEVKITC)
+// // Rotary encoder pin
+// #define ENCODER_SW_PIN 42
+// #define ENCODER_DT_PIN 41
+// #define ENCODER_CLK_PIN 40
 
-// TFT
-#define TFT_MISO 38
-#define TFT_MOSI 18
-#define TFT_SCLK 21
-#define TFT_CS 15
-#define TFT_DC 17
-#define TFT_RST 16
-#define TFT_BL 46
-
-// Sensors / actuators
-#define SDA_PIN 8
-#define SCL_PIN 9
-#define SERVO_VALVE_PIN 4
-#define PUMP_1_PIN 5
-#define PUMP_2_PIN 6
-#define GEAR_MOTOR_PIN 7
-#define TRIG_1_PIN 10
-#define ECHO_1_PIN 11
-#define TRIG_2_PIN 12
-#define ECHO_2_PIN 13
-#define DQ_PIN 14
-
-// Rotary encoder pin
-#define ENCODER_SW_PIN 42
-#define ENCODER_DT_PIN 41
-#define ENCODER_CLK_PIN 40
-
-#elif defined(BOARD_ESP32_DEVKIT)
-
-// TFT
-#define TFT_MISO 19
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS 5
-#define TFT_DC 2
-#define TFT_RST 4
-#define TFT_BL -1
-
+// ESP32
 // Sensors / actuators
 #define SDA_PIN 21
 #define SCL_PIN 22
@@ -57,7 +36,3 @@
 #define ENCODER_SW_PIN 15
 #define ENCODER_DT_PIN 33
 #define ENCODER_CLK_PIN 32
-
-#else
-#error "Unsupported board target"
-#endif
